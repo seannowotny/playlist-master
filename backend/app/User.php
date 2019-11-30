2bc0397affcfec3780a323414093419ceb62274e
SNx42 <seannowotny@live.de>
2019-11-30 13:04:40 +1000

    Sketch additional User fields

--- backend/app/User.php
+++ backend/app/User.php
@@ -10,6 +10,21 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    // function playlists()
+    // {
+    //     return $this->hasMany(Playlist::class);
+    // }
+
+    // function likes()
+    // {
+    //     return $this->hasMany(Like::class);
+    // }
+
+    // function comments()
+    // {
+    //     return $this->hasMany(Comment::class);
+    // }
+
     /**
      * The attributes that are mass assignable.
      *
